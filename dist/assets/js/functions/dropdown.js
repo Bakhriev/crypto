55daadbc1d9749c32d34eb5234207d1cd0a89787
Bakhriev <legendofpain5112@gmail.com>
2023-05-17 18:33:23 +0500

    header and hero

---
+++ dist/assets/js/functions/dropdown.js
@@ -0,0 +1,12 @@
+document.addEventListener('click', (e) => {
+  let currentDropdown
+  if (e.target.closest('[data-dropdown]')) {
+    currentDropdown = e.target.closest('[data-dropdown]')
+    currentDropdown.classList.toggle('active')
+  }
+
+  document.querySelectorAll('[data-dropdown].active').forEach((dropdown) => {
+    if (dropdown === currentDropdown) return
+    dropdown.classList.remove('active')
+  })
+})
--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,1 +1,28 @@
 import './functions/burger.js'
+
+const dropdowns = document.querySelectorAll('[data-dropdown]')
+
+document.addEventListener('click', (e) => {
+  let currentDropdown
+  if (e.target.closest('[data-dropdown]')) {
+    currentDropdown = e.target.closest('[data-dropdown]')
+    currentDropdown.classList.toggle('active')
+    onClickDropdown()
+  }
+
+  document.querySelectorAll('[data-dropdown].active').forEach((dropdown) => {
+    if (dropdown === currentDropdown) return
+    dropdown.classList.remove('active')
+  })
+})
+
+function onClickDropdown() {
+  dropdowns.forEach((dropdown) => {
+    const value = dropdown.querySelector('[data-dropdown-value]')
+    dropdown.querySelectorAll('.picker__menu-item').forEach((item) => {
+      item.addEventListener('click', () => {
+        value.textContent = item.textContent
+      })
+    })
+  })
+}
